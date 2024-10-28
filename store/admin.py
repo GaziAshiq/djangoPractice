b515bc529f9d66401d17860e15e904b13c6a51ee
Gazi Ashiq <GaziAshiq.Dev@gmail.com>
2024-10-28 21:41:36 +0600

    showing related objects to an admin panel

--- store/admin.py
+++ store/admin.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
-from .models import Collection, Customer, Product, Promotion
+from .models import Collection, Customer, Product, Promotion, Order, OrderItem
 
 
 # Register your models here.
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'price', 'inventory_status']
+    list_display = ['title', 'price', 'inventory_status', 'collection']
     list_editable = ['price']
     list_per_page = 10
+    list_select_related = ['collection']  # this will reduce the number of queries for a foreign key
 
-    @admin.display(ordering='inventory') # sorting by inventory
+    @admin.display(ordering='inventory')  # sorting by inventory
     def inventory_status(self, product):
         # here we are checking the inventory of product and returning the status
         if product.inventory < 10:
@@ -23,6 +24,13 @@ class CustomerAdmin(admin.ModelAdmin):
     list_editable = ['membership']
     ordering = ['first_name', 'last_name']
     list_per_page = 10
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'customer', 'placed_at']
+    list_per_page = 10
+    list_select_related = ['customer']
 
 
 admin.site.register(Collection)
