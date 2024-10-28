6acd27072d215d0447aa40a7a69afaa06ee617ad
Gazi Ashiq <GaziAshiq.Dev@gmail.com>
2024-10-28 21:18:35 +0600

    show collection, product to admin panel with sorted title

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Collection, Customer, Product, Promotion
 
 # Register your models here.
+admin.site.register(Product)
+admin.site.register(Collection)
--- store/models.py
+++ store/models.py
@@ -21,6 +21,13 @@ class Collection(models.Model):
     featured_product = models.ForeignKey(
         'Product', on_delete=models.SET_NULL, null=True, related_name='+', blank=True)
 
+    def __str__(self):
+        return self.title # this will show title in admin panel
+
+    class Meta:
+        # ordering by title in ascending order
+        ordering = ['title']
+
 
 class Product(models.Model):
     title = models.CharField(max_length=64)
@@ -34,6 +41,12 @@ class Product(models.Model):
     # many-to-one, Product belongs to one Collection, but a Collection can have multiple Products.
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions = models.ManyToManyField(Promotion)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']
 
 
 class Customer(models.Model):
