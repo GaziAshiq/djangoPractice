498b5fe01e84826ddea10bb64cbc2d35c0d5dcac
Gazi Ashiq <GaziAshiq.Dev@gmail.com>
2024-10-28 21:34:24 +0600

    adding computed column to product admin

--- store/admin.py
+++ store/admin.py
@@ -5,10 +5,16 @@ from .models import Collection, Customer, Product, Promotion
 # Register your models here.
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'price']
+    list_display = ['title', 'price', 'inventory_status']
     list_editable = ['price']
     list_per_page = 10
 
+    @admin.display(ordering='inventory') # sorting by inventory
+    def inventory_status(self, product):
+        # here we are checking the inventory of product and returning the status
+        if product.inventory < 10:
+            return 'Low'
+        return 'OK'
 
 
 @admin.register(Customer)
